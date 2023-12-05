3ffd491e4d908942cf2b1ae02fb968ac7b3f9ca3
Kai Siang Kao <kaokaisiang021001@gmail.com>
2023-12-05 14:27:50 +1100

    Refactored kanban-column to accept object

--- src/app/(pages)/[projectId]/tasks/page.tsx
+++ src/app/(pages)/[projectId]/tasks/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import AddColumnButton from "~/components/add-column-button";
-import TaskColumn from "~/components/task-column";
+import TaskColumn from "~/components/kanban-column";
 import { api } from "~/trpc/server";
 
 export default async function TasksPage({
@@ -9,12 +9,13 @@ export default async function TasksPage({
 	params: { projectId: string };
 }) {
 	const columns = await api.kanban.getColumns.query(params.projectId);
+
 	return (
 		<div className="max-h-full h-full">
 			<h1 className="text-2xl font-bold">Task Board</h1>
 			<div className="pt-4 max-h-[calc(100%-2rem)] h-full flex gap-x-3 w-fit pr-8">
 				{columns.map((c) => (
-					<TaskColumn key={c.id} columnName={c.name} />
+					<TaskColumn key={c.id} column={c} />
 				))}
 				<AddColumnButton />
 			</div>
--- src/components/kanban-column.tsx
+++ src/components/kanban-column.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import type { inferRouterOutputs } from "@trpc/server";
+import type { AppRouter } from "~/server/api/root";
 
-export default function TaskColumn({ columnName }: { columnName: string }) {
+type Props = inferRouterOutputs<AppRouter>["kanban"]["getColumns"][number];
+
+export default function TaskColumn({ column }: { column: Props }) {
 	return (
 		<div className="min-w-[16rem] h-fit max-h-full bg-input dark:bg-primary-foreground w-64 p-2 border dark:border-0 rounded-md overflow-y-auto">
 			<h2 className="font-bold p-1 whitespace-nowrap max-w-full overflow-ellipsis overflow-hidden">
-				{columnName}
+				{column.name}
 			</h2>
 			<div className="w-full h-32 mt-2 bg-primary-foreground dark:bg-input"></div>
 			<div className="w-full h-32 mt-2 bg-primary-foreground dark:bg-input"></div>
