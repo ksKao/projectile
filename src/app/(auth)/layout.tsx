6381585e49ce0e5017a59cfa01c3c5135141f9aa
Kai Siang Kao <kaokaisiang021001@gmail.com>
2023-12-09 21:35:09 +1100

    Added redirect in auth pages if user is logged in

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,5 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 import { Card, CardContent } from "~/components/ui/card";
 
@@ -6,6 +8,10 @@ export default function AuthLayout({
 }: {
 	children: React.ReactNode;
 }) {
+	const { user } = auth();
+
+	if (user) redirect("/");
+
 	return (
 		<Card className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pt-4 w-96 max-w-[80%]">
 			<CardContent>{children}</CardContent>
