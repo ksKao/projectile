90364dd00a6a12e86257df5ce873f3ecf52157c7
Kai Siang Kao <kaokaisiang021001@gmail.com>
2023-12-10 22:56:00 +1100

    Added number of replies to thread card

--- src/app/(pages)/[projectId]/threads/thread-card.tsx
+++ src/app/(pages)/[projectId]/threads/thread-card.tsx
@@ -2,15 +2,12 @@
 import React from "react";
 import type { inferRouterOutputs } from "@trpc/server";
 import type { AppRouter } from "~/server/api/root";
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardTitle,
-} from "~/components/ui/card";
-import { formatDistanceStrict } from "date-fns";
+import { Card, CardContent, CardTitle } from "~/components/ui/card";
+import { formatDistance } from "date-fns";
 import { useProject } from "~/lib/contexts/projectContext";
-import { Avatar, AvatarImage } from "~/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import { FaRegClock, FaRegComment } from "react-icons/fa6";
+import { FaRegUserCircle } from "react-icons/fa";
 
 type ThreadWithNumberOfReplies =
 	inferRouterOutputs<AppRouter>["threads"]["getThreads"][number];
@@ -24,31 +21,44 @@ export default function ThreadCard({
 
 	const author = project.members.find((m) => m.id === thread.author);
 
-	if (!author) throw new Error("Author of post not found in member.");
-
 	return (
 		<Card className="p-4 max-w-[calc(100vw-17rem-64px)] mt-4">
 			{/* 17 rem = width of sidebar, 64 px = padding of side bar, 64px = padding of content */}
 			<div className="flex gap-4 items-center">
 				<Avatar>
-					<AvatarImage src={author.imageUrl} />
+					<AvatarImage src={author?.imageUrl} />
+					<AvatarFallback>N/A</AvatarFallback>
 				</Avatar>
 				<div className="max-w-full min-w-0">
 					<CardTitle className="mb-1 truncate w-full">
 						{thread.title}
 					</CardTitle>
-					<CardDescription>
-						{formatDistanceStrict(thread.createdAt, Date.now(), {
-							addSuffix: true,
-						})}{" "}
-						· {author.username}
-					</CardDescription>
 				</div>
 			</div>
 			<CardContent className="p-0 mt-4 max-w-full">
-				<p className="truncate min-w-0 text-muted-foreground">
+				<p className="truncate min-w-0">
 					{thread.content.replace(/<[^>]*>/g, "")}
 				</p>
+				<div className="items-center flex gap-6 text-muted-foreground mt-4">
+					<span className="inline-flex items-center gap-2">
+						<FaRegUserCircle />
+						{author?.username ?? "Removed"}
+					</span>
+					<span className="inline-flex items-center gap-2">
+						<FaRegClock />
+						{formatDistance(thread.createdAt, Date.now(), {
+							addSuffix: true,
+						})
+							.split("")
+							.map((char, index) =>
+								index === 0 ? char.toUpperCase() : char,
+							)
+							.join("")}
+					</span>
+					<span className="inline-flex items-center gap-2">
+						<FaRegComment /> {thread.numberOfReplies}
+					</span>
+				</div>
 			</CardContent>
 		</Card>
 	);
