2c69fb46885e086c6cbcc0cf4e87a324b2129a47
Kai Siang Kao <kaokaisiang021001@gmail.com>
2023-12-12 23:56:44 +1100

    Fixed deployment error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,6 @@ import { TRPCReactProvider } from "~/trpc/react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "./theme-provider";
 import { Toaster } from "react-hot-toast";
-import NextTopLoader from "nextjs-toploader";
 import ProgressBarProvider from "~/lib/progressBarProvider";
 
 const inter = Inter({
@@ -32,7 +31,6 @@ export default function RootLayout({
 		<ClerkProvider>
 			<html lang="en" suppressHydrationWarning>
 				<body className={`font-sans ${inter.variable} bg-background`}>
-					{/* <NextTopLoader /> */}
 					<ProgressBarProvider>
 						<ThemeProvider
 							attribute="class"
