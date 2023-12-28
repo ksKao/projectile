49763c97800751ab484f262585f082294bea93d4
Kai Siang Kao <kaokaisiang021001@gmail.com>
2023-12-28 13:56:02 +0800

    Fixed settings page overflow issue

--- src/app/(pages)/settings/page.tsx
+++ src/app/(pages)/settings/page.tsx
@@ -5,12 +5,12 @@ export default async function Settings() {
 	const user = await currentUser();
 
 	return (
-		<>
+		<div className="max-h-full overflow-y-auto -mr-12">
 			<h1 className="font-bold text-3xl">Account Settings</h1>
 			<SettingsForm
 				defaultUsername={user?.username ?? ""}
 				defaultImageUrl={user?.imageUrl ?? ""}
 			/>
-		</>
+		</div>
 	);
 }
