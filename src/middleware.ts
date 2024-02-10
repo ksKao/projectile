61cb751f065f43ed35adca99e43547fe801b9c38
Kao Kai Siang <kaokaisiang021001@gmail.com>
2024-02-10 09:23:32 +0800

    Revert "Updated clerk public routes"
    This reverts commit 2dc901948f97bdef9fa6763c9c74939f124fa7b8.

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,10 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-	publicRoutes: ["/password-reset", "/api/trpc(.*)"],
+	publicRoutes: ["/password-reset", "/api/(.*)"],
+	afterAuth: () => {
+		return;
+	},
 });
 
 export const config = {
